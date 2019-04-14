41c5136d125cb945216bc2c1fa252b0ce9daf2a2
jczetta <jczetta@gmail.com>
2019-04-14 16:48:24 -0400

    Initial commit of app file and csv data

---
+++ app.py
@@ -0,0 +1,21 @@
+
+#__author__ == "Jackie Cohen (jczetta)"
+
+import os
+from flask import Flask, render_template, session, redirect, url_for # tools that will make it easier to build on things
+from flask_sqlalchemy import SQLAlchemy # handles database stuff for us - need to pip install flask_sqlalchemy in your virtual env, environment, etc to use this and run this
+
+# Application configurations
+app = Flask(__name__)
+app.debug = True
+app.use_reloader = True
+app.config['SECRET_KEY'] = 'hard to guess string for app security adgsdfsadfdflsdfsj'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./sample_songs.db' # TODO: decide what your new database name will be -- that has to go here
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Set up Flask debug stuff
+db = SQLAlchemy(app) # For database use
+session = db.session # to make queries easy
